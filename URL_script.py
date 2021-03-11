d5bdf9024d568ce74da1afb2b4ee96ee2f86de89
unknown <giacomo.fumagalli1@studenti.unimi.it>
2021-03-11 16:15:07 +0100

    first commit

---
+++ URL_script.py
@@ -0,0 +1,23 @@
+# Estrae solo URL da dataset
+
+import numpy as np
+
+# Numero di entry
+n  = 100
+# Legit o phishing, 'L' o 'P'
+s = 'L'
+# Path della directory principale del dataset
+path = 'C:/Users/Giacomo/Downloads/Legitimate1/'
+
+# Genero nomi delle directory del tipo 'L00001', 'L00002',...
+num = ['%d' % i for i in range(n)]
+dirs = [s + '0'*(5 - len(num[i])) + str(i+1) for i in range (n)]
+
+data = np.array([])
+
+for dirname in dirs:
+    with open(path + dirname + '/URL/URL.txt', 'r') as outfile:
+        data = np.append(data, outfile.read())
+        outfile.close()
+
+np.save('data.npy', data)
---
+++ filters/CNN.py
@@ -0,0 +1,145 @@
+from torch.utils.data.sampler import SubsetRandomSampler
+import torch.optim as optim
+import time
+from torch.autograd import Variable
+import torch.distributions as ds
+import numpy as np
+import torch
+from tqdm import tqdm_notebook as tqdm
+import torch.nn as nn
+import torch.utils.data as data
+import matplotlib.pyplot as plt
+import torch.utils.data.sampler as sampler
+import torch.nn.functional as F
+import seaborn as sns
+
+class CNN(torch.nn.Module):
+        
+    def __init__(self):
+        super(CNN, self).__init__()
+        
+        #Input channels = 1, output channels = 18
+        self.conv1 = torch.nn.Conv2d(1, 50, kernel_size=3, stride=1, padding=1)
+        self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        
+        #4608 input features, 64 output features (see sizing flow below)
+        self.fc1 = torch.nn.Linear(50 * 14 * 14, 100)
+        
+        #64 input features, 10 output features for our 10 defined classes
+        self.fc2 = torch.nn.Linear(100, 2)
+        
+    def forward(self, x):
+        #Computes the activation of the first convolution
+        #Size changes from (3, 32, 32) to (18, 32, 32)
+        x = F.relu(self.conv1(x))
+        
+        #Size changes from (18, 32, 32) to (18, 16, 16)
+        x = self.pool(x)
+        
+        #Reshape data to input to the input layer of the neural net
+        #Size changes from (18, 16, 16) to (1, 4608)
+        #Recall that the -1 infers this dimension from the other given dimension
+        x = x.view(-1, 50 * 14 *14)
+        
+        #Computes the activation of the first fully connected layer
+        #Size changes from (1, 4608) to (1, 64)
+        x = F.relu(self.fc1(x))
+        
+        #Computes the second fully connected layer (activation applied later)
+        #Size changes from (1, 64) to (1, 10)
+        x = self.fc2(x)
+        return(x)
+
+def createLossAndOptimizer(net, learning_rate=0.001):
+    
+    #Loss function
+    loss = torch.nn.CrossEntropyLoss()
+    
+    #Optimizer
+    optimizer = optim.Adam(net.parameters(), lr=learning_rate)
+    
+    return(loss, optimizer)
+
+def trainNet(net, train_loader, val_loader, batch_size, n_epochs, learning_rate):
+    
+    #Print all of the hyperparameters of the training iteration:
+    # print("===== HYPERPARAMETERS =====")
+    # print("batch_size=", batch_size)
+    # print("epochs=", n_epochs)
+    # print("learning_rate=", learning_rate)
+    # print("=" * 30)
+    
+    #Get training data
+    n_batches = len(train_loader)
+    
+    #Create our loss and optimizer functions
+    loss, optimizer = createLossAndOptimizer(net, learning_rate)
+    
+    #Time for printing
+    training_start_time = time.time()
+    
+    #Loop for n_epochs
+    for epoch in range(n_epochs):
+        
+        running_loss = 0.0
+        print_every = n_batches // 10
+        start_time = time.time()
+        total_train_loss = 0
+        
+        for i, data in enumerate(train_loader, 0):
+            
+            #Get inputs
+            inputs, labels = data
+            #print labels
+            
+            #Wrap them in a Variable object
+            inputs, labels = Variable(inputs), Variable(labels)
+            
+            #Set the parameter gradients to zero
+            optimizer.zero_grad()
+            
+            #Forward pass, backward pass, optimize
+            #print inputs.shape
+            outputs = net(inputs)
+            #print outputs
+            loss_size = loss(outputs, labels)
+            loss_size.backward()
+            optimizer.step()
+            
+            #Print statistics
+            running_loss += loss_size.data.item()
+            total_train_loss += loss_size.data.item()
+            
+            #Print every 10th batch of an epoch
+            if (i + 1) % (print_every + 1) == 0:
+                # print("Epoch {}, {:d}% \t train_loss: {:.2f} took: {:.2f}s".format(
+                #         epoch+1, int(100 * (i+1) / n_batches), running_loss / print_every, time.time() - start_time))
+                #Reset running loss and time
+                running_loss = 0.0
+                start_time = time.time()
+            
+        #At the end of the epoch, do a pass on the validation set
+        total_val_loss = 0
+        for inputs, labels in val_loader:
+            
+            #Wrap tensors in Variables
+            inputs, labels = Variable(inputs), Variable(labels)
+            
+            #Forward pass
+            val_outputs = net(inputs)
+            val_loss_size = loss(val_outputs, labels)
+            total_val_loss += val_loss_size.data.item()
+            
+        # print("Validation loss = {:.2f}".format(total_val_loss / len(val_loader)))
+        
+    print("Training finished, took {:.2f}s".format(time.time() - training_start_time))
+
+def createLossAndOptimizer(net, learning_rate=0.001):
+    
+    #Loss function
+    loss = torch.nn.CrossEntropyLoss()
+    
+    #Optimizer
+    optimizer = optim.Adam(net.parameters(), lr=learning_rate)
+    
+    return(loss, optimizer)
