59362f386417a66622932ef8ed359f42f9035c1b
Davide Raimondi <daduraimondi@gmail.com>
2021-04-22 11:22:30 +0200

    correzione model selection+cutoff

--- local_exec/classificatori.py
+++ local_exec/classificatori.py
@@ -2,122 +2,164 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.metrics import classification_report
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.model_selection import train_test_split,GridSearchCV
-from sklearn.linear_model import SGDClassifier,LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer,  TfidfVectorizer
+from sklearn.model_selection import train_test_split, GridSearchCV , StratifiedKFold
+from sklearn.linear_model import SGDClassifier, LogisticRegression
 import sklearn
+from sklearn.model_selection import KFold
 import numpy as np
 import pandas as pd
 import init
 
 
-def generic_classifier(X_train,y_train,X_test,y_test,classifier,name,verbose=True):  #classe per fare training e valutare il classificatore
+def train_and_fit(X_train, y_train, X_test, y_test, classifier, name, verbose=True):  #classe per fare training e valutare il classificatore  -> train and set
     nb = Pipeline([
-                ('tfidf', TfidfTransformer()),                  #tfidf, va ad assegnare un vettore dei pesi in base alla composizione del testo 
-                ('clf', classifier),                            #applica classificatore passato come parametro
+                ('tfidf',  TfidfTransformer()),                   #tfidf,  va ad assegnare un vettore dei pesi in base alla composizione del testo 
+                ('clf',  classifier),                             #applica classificatore passato come parametro
                 ])
-    nb.fit(X_train, y_train)                                    #tfidf su X_train e training del classificatore
+    nb.fit(X_train,  y_train)                                    #tfidf su X_train e training del classificatore
     y_pred = nb.predict(X_test)                                 #tfidf su X_test e calcollo di y pred
     if(verbose):
         print(f"{name}:")
-        print('accuracy %s' % sklearn.metrics.accuracy_score(y_test,y_pred))
-        print(classification_report(y_test, y_pred))
-    return classification_report(y_test,y_pred,output_dict=True)    # return classification report
+        print('accuracy %s' % sklearn.metrics.accuracy_score(y_test, y_pred))
+        print(classification_report(y_test,  y_pred))
+    return classification_report(y_test, y_pred, output_dict=True,target_names=["Legitimate","Phishing"])    # return classification report
 
 def Bayes_clasifier(alpha_params=1):       
     return MultinomialNB(alpha=alpha_params)
-
-def SVM_classifier(C_param=1.0,penality_param='l2',loss_param = 'squared_hinge'):
-    return sklearn.svm.LinearSVC(C=C_param,penalty=penality_param,loss=loss_param,max_iter=10000)
+#definisco all'inizio griglia val. fissi parametri; per classificatore dizionario che associa a params fissi valori fissi
+def SVM_classifier(C_param=1.0, penality_param='l2', loss_param = 'squared_hinge'):
+    return sklearn.svm.LinearSVC(C=C_param, penalty=penality_param, loss=loss_param, max_iter=10000)
     
-def LogisticRegression_classifier(C_param=1,max_iter_param=2000,solver_param='lbfgs'):
-    return LogisticRegression(C=C_param,max_iter=max_iter_param,solver=solver_param)
+def LogisticRegression_classifier(C_param=1, max_iter_param=2000, solver_param='lbfgs'):
+    return LogisticRegression(C=C_param, max_iter=max_iter_param, solver=solver_param)
 
-def get_set(sbilanciamento,codif=None):
-        X,y = init.load_data(sbilanciamento) #prendo i dati (argomento uno va ad indicare il ratio fra phishing e legitimate
+def get_set(codif=None,cutoff=None):
+        X, y = init.load_data() #prendo i dati (argomento uno va ad indicare il ratio fra phishing e legitimate
                                                         #ho scelto di usare un dataset bilanciato)
         X =X.tolist()
-        y=y.tolist()
-        X_train, X_test, y_train, y_test =train_test_split(X,y,test_size=0.3)    #divido Train e test set
-        if(len(X_train) != len(list(set(X_train)-set(X_test)))):                #assertion error in caso di elementi presenti sia in X_train che in X_test
-            raise AssertionError()
-        if(codif==None):                                                        #default  ritorna X_train,y_train...
-            return X_train,y_train,X_test,y_test
+        if(cutoff):
+            for i,x in enumerate(X):
+                X[i]= x[:min(len(x),cutoff)]
+            pX = []
+            py = []
+            x_prev = ""
+            xy =  [(x,y) for x,y in zip(X,y)]
+            for x,y in sorted(xy,key=lambda x:x[0]):                #ciclo per eliminare elementi doppi (X) e relative y dopo il cutoff
+                if(x!=x_prev):
+                    pX.append(x)
+                    py.append(y)
+                x_prev=x
+            X = pX
+            y = py
+        X_train,  X_test,  y_train,  y_test =train_test_split(X, y, test_size=0.3)    #divido Train e test set
+        assert(len(X_train) == len(list(set(X_train)-set(X_test))))             #assertion error in caso di elementi presenti sia in X_train che in X_test
+       
+        if(codif==None):                                                        #default  ritorna X_train, y_train...
+            return X_train, y_train, X_test, y_test
+
         codif.fit(X)                                                            #creo dizionario con le parole
         X_train= codif.transform(X_train)                                       #applico codifica (bag of word/bag of char) su dataset per intero
         X_test = codif.transform(X_test)
-        return X_train,y_train,X_test,y_test                                    #ritorna X_train,X_test codificati
+        return X_train, y_train, X_test, y_test                                    #ritorna X_train, X_test codificati
 
-def makedf(list,metrics,names,iter= False):                                    #stampa DF con risultati ed salva in latex
+def makedf(list, metrics, names, iter= False):                                    #stampa DF con risultati ed salva in latex
     diz= {}
     if (iter == True):
         for name in names:
             for metric in metrics:
                 diz[metric] ={
-                    'min': np.amin(list[name][metric]),
-                    'max':np.amax(list[name][metric]),
-                    'avg':np.average(list[name][metric]),
+                    'min': np.amin(list[name][metric]), 
+                    'max':np.amax(list[name][metric]), 
+                    'avg':np.average(list[name][metric]), 
                     'dev_std':np.std(list[name][metric])
                 }
             dframe = pd.DataFrame(diz)
-            dframe.to_latex(buf=f"/home/dav/Scrivania/latex/{name}.tex") 
+            dframe.to_latex(buf=f"/home/dav/Scrivania/latex/{name}char.tex") 
             print(pd.DataFrame(diz))
     else:
         for name in names:
             for metric in metrics:
                 diz[metric] = list[name][metric]
             dframe = pd.DataFrame(diz)
-            dframe.to_latex(buf=f"/home/dav/Scrivania/latex/{name}es1.tex")
+            dframe.to_latex(buf=f"/home/dav/Scrivania/latex/{name}char.tex")
             print(pd.DataFrame(diz))
 
-def  analysis(iteration,namelist,classifiers,codif,verbose=False,sbilanciamento=1,ts=None):     #funzione per andare a effettuare una o più iterazioni dati i classificatori
-    metrics = ['precision','recall','f1-score','accuracy']
+def  analysis(iteration, namelist, classifiers, codif, verbose=False, ts=None):     #funzione per andare a effettuare una o più iterazioni dati i classificatori
+    metrics = ['precision', 'recall', 'f1-score', 'accuracy']
     rlist = {}
     for name in namelist:
         rlist[name]= {}
         for metric in metrics:
             rlist[name][metric] = []
 
-    for i in range(iteration):
+    for _ in range(iteration):
         if(ts == None):
-            X_train,y_train,X_test,y_test=get_set(sbilanciamento,codif)
+            X_train,  y_train,  X_test,  y_test = get_set(codif)
         else:
-            X_train,y_train,X_test,y_test=ts
-        for j,classifier,name in zip(range(0,len(classifiers)),classifiers,namelist):
-            res=generic_classifier(X_train,y_train,X_test,y_test,classifier,name,False)
-            for k,metric in zip(range(0,len(metrics)),metrics):
+            X_train, y_train, X_test, y_test= ts
+        for classifier, name in zip(classifiers, namelist):
+            res=train_and_fit(X_train, y_train, X_test, y_test, classifier, name, verbose)
+            for metric in metrics:
                 if(metric == 'accuracy'):
-                    rlist[name][metric]+=[(res[metric])]
+                    rlist[name][metric]+= [(res[metric])]
                 else:  
-                    rlist[name][metric]+=[res['weighted avg'][metric]]
-    makedf(rlist,metrics,namelist,(iteration>1))
+                    rlist[name][metric]+= [res['Phishing'][metric]]
 
-def GridModelSelection(sbilanciamento,codifica,estimator,params,name):
-    X_train,y_train,X_test,y_test =get_set(sbilanciamento)
-    X_trainval,X_validation,y_trainval,y_validation= train_test_split(X_train,y_train,test_size=0.3)
-    codifica.fit(X_train+X_test)
-    X_trainval=codifica.transform(X_trainval)
-    X_validation = codifica.transform(X_validation)
-    X_train = codifica.transform(X_train)
-    X_test = codifica.transform (X_test)
-    gridmodel = GridSearchCV(estimator,params,scoring='f1_weighted')
-    gridmodel.fit(X_trainval,y_trainval)
-    print(gridmodel.best_params_)
-    analysis(1,[name],[gridmodel.best_estimator_],codifica,ts=(X_train,y_train,X_test,y_test))
+    makedf(rlist, metrics, namelist, (iteration>1))
 
+def GridModelSelection(codifica, estimator, params, name, nmparams):
+    #divisone partizioni
+    X,y = init.load_data(False)
+    X = np.array(X)
+    y = np.array(y)
+    codifica.fit(X)
+    kf=StratifiedKFold(n_splits=5)
+    rlist={ nmparams: [],
+           'accuracy':[],
+           'f1-score':[]}
+    for train,test in kf.split(X,y):
+        print(len(test))
+        X_test, X_train = X[test], X[train]
+        y_test, y_train = y[test], y[train]
+        assert(len(X_test)==len(list(set(X_test)-set(X_train))))
+        X_train = codifica.transform(X_train)
+        X_test = codifica.transform(X_test)
+        gridmodel = GridSearchCV(estimator, params, scoring='f1_macro', cv=5, return_train_score=True)          #creo oggetto GridSearch
+        gridmodel.fit(X_train, y_train)                                      #effettuo ricerca su X_trainval
+        print(gridmodel.best_params_)                                             #stampo best param
+        res = train_and_fit(X_train, y_train, X_test, y_test, gridmodel.best_estimator_, name, False)
+        rlist[nmparams] += [gridmodel.best_params_[nmparams]]
+        rlist['accuracy'] += [res['accuracy']]
+        rlist['f1-score'] += [res['Phishing']['f1-score']]
+    dfparam = pd.DataFrame(rlist)
+    metrics=['f1-score','accuracy']
+    valutazione = {}
+    for metric in metrics:
+        valutazione[metric] = {'avg' : np.average(rlist[metric]),
+                                'std': np.std(rlist[metric])}
 
-def Bayes_params(start,end,granularity):
-    return {'alpha' : np.arange(start,end,granularity)}
-def SMV_params(start,end,granularity):
-    return {'C' : np.arange(start,end,granularity)}
+    dfval = pd.DataFrame(valutazione)
+    dfparam.to_latex(buf=f"/home/dav/Scrivania/latex/{name}wordparams-score.tex")
+    dfval.to_latex(buf=f"/home/dav/Scrivania/latex/{name}wordvalidation.tex")
+    print(dfparam)
+    print(dfval)
+  
+
+def Bayes_params(start, end):
+    print(np.logspace(start, end, (end-start)))
+    return {'alpha' : np.logspace(start, end, (end-start))}
+def SMV_params(start, end, granularity):
+    return {'C' : np.arange(start, end, granularity)}
 def Logistic_params():
-    return {'C' : [10,100,1000,10000,100000,1000000]}
+    return {'C' : [10, 100, 1000, 10000, 100000, 1000000]}
 
 if __name__ == "__main__":
-    codif =  CountVectorizer(analyzer='char')
-    namelist = ["Naive Bayes","SVM","Logistic regression"]
-    classifiers = [Bayes_clasifier(),SVM_classifier(),LogisticRegression_classifier()]
-    # GridModelSelection(0,codif,Bayes_clasifier(),Bayes_params(0.1,5,0.1),"Naive Bayes")
-    #GridModelSelection(0,codif,LogisticRegression_classifier(),Logistic_params(),"Logistic Regression")
-    #GridModelSelection(0,codif,SVM_classifier(),SMV_params(0.1,2,0.1),"Linear SVM")
-    analysis(10,["Logistic regression"],[LogisticRegression_classifier(C_param=1000)],codif)
+    codif =  CountVectorizer(analyzer='word')
+    namelist = ["Naive Bayes", "SVM", "Logistic regression"]
+    classifiers = [Bayes_clasifier(), SVM_classifier(), LogisticRegression_classifier()]
+    GridModelSelection(codif, Bayes_clasifier(), Bayes_params(-10,10), "Naive bayes","alpha")
+    #GridModelSelection(codif, LogisticRegression_classifier(), Logistic_params(), "Logistic Regression","C")
+    #GridModelSelection(codif, SVM_classifier(), SMV_params(0.1, 0.5, 0.1), "Linear SVM")
+    #analysis(10, namelist,classifiers, codif,verbose=False) 
+       
